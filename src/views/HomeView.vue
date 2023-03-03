61813617d0e84537a55f36c715abdc698bcab3ef
Tionebl <blavoine@icloud.com>
2023-03-04 00:33:48 +0100

    button

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,13 @@
 <script setup>
 import typeWriter from '@/components/typeWriter/typeWriter.vue'
+const pdfUrl = "public/CV_Oct2022_BenoitLavoine.pdf";
+
+const downloadPdf = () => {
+  const link = document.createElement('a');
+  link.href = pdfUrl;
+  link.download = 'CV_Oct2022_BenoitLavoine.pdf';
+  link.click();
+}
 </script>
 <template>
   <div class = page>
@@ -11,6 +19,7 @@ import typeWriter from '@/components/typeWriter/typeWriter.vue'
       <div class="infoContainer">
         <typeWriter class="typeApp"></typeWriter>
         <h3 class="leftApparition"> Hey there! I'm a web developer based in Lille. With a passion for building dynamic and interactive web applications, I specialize in using Javascript and Vue 3 to create seamless user experiences. Whether it's designing responsive websites or crafting complex web applications, I love to bring my ideas to life through code. Thanks for stopping by my portfolio!</h3>
+        <button @click="downloadPdf">Download CV</button>
       </div>
     </div>
   </div>
@@ -33,6 +42,25 @@ import typeWriter from '@/components/typeWriter/typeWriter.vue'
  border-left: solid 1px gray;
  min-width: 40vh;
 
+}
+
+button {
+
+  margin: 30px;
+  width: 20%;
+  height: 12%;
+  color: white;
+  font-weight: bold;
+  background-color:#04AA6D;
+  border:none;
+  cursor: pointer;
+}
+
+button:hover{
+
+  transform: scale(1.1);
+  transition: all .3s ease-in-out;
+  background-color: #009860;
 }
 
 img {
@@ -139,4 +167,22 @@ min-width: 5vh;
 
 
 }
+
+@media screen and (max-width: 390px) {  
+
+  .container{
+    margin: 0px;
+    position: fixed;
+    transform: translateX(-55%);
+  }
+  button{
+
+    position: fixed;
+    height: 9.5%;
+    padding: 1px;
+    top: 88%;
+    left: 70%;
+  }
+}
+
 </style>
