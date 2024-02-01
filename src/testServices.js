c7ef866c73f00c62a40c12e4c4a704e076487543
Tionebl <blavoine@icloud.com>
2024-02-01 01:06:33 +0100

    a

--- src/testServices.js
+++ src/testServices.js
@@ -3,50 +3,50 @@ const { exec } = require('child_process');
 const nomDuService = 'wuauserv';
 
 // Commande pour démarrer le service Windows
-const commande = net start ${nomDuService};
+const commande = `net start ${nomDuService}`;
 
-const isActivated = sc query ${nomDuService};
+const isActivated = `sc query ${nomDuService}`;
 
-const stopSvc = net stop ${nomDuService};
+const stopSvc = ` net stop ${nomDuService}`;
 
-const svcList = sc query ${nomDuService};
+const svcList = `sc query ${nomDuService}`;
 // // Exécution de la commande
 // exec(commande, (erreur, stdout, stderr) => {
 //     if (erreur) {
-//         console.error(Erreur lors du démarrage du service : ${erreur});
+//         console.error(`Erreur lors du démarrage du service : ${erreur}`);
 //         return;
 //     }
-//     console.log(Service ${nomDuService} démarré avec succès);
-//     console.log(Sortie standard : ${stdout});
-//     console.error(Sortie d'erreur : ${stderr});
+//     console.log(`Service ${nomDuService} démarré avec succès`);
+//     console.log(`Sortie standard : ${stdout}`);
+//     console.error(`Sortie d'erreur : ${stderr}`);
 // });
 
 exec(isActivated, (erreur, stdout, stderr) => {
-    console.log( ${nomDuService} est bien activé)
+    console.log( `${nomDuService} est bien activé`)
 });
 
 // exec(stopSvc, (erreur, stdout, stderr) => {
 //     if (erreur) {
-//         console.error(Erreur lors de l'arrêt du service : ${erreur});
+//         console.error(`Erreur lors de l'arrêt du service : ${erreur}`);
 //         return;
 //     }
 
 //     if (stdout.includes('Arrêt du service réussi')) {
-//         console.log(Service ${nomDuService} arrêté avec succès);
+//         console.log(`Service ${nomDuService} arrêté avec succès`);
 //     } else {
-//         console.log(L'arrêt du service ${nomDuService} a échoué);
+//         console.log(`L'arrêt du service ${nomDuService} a échoué`);
 //     }
 
-//     console.log(Sortie standard : ${stdout});
-//     console.error(Sortie d'erreur : ${stderr});
+//     console.log(`Sortie standard : ${stdout}`);
+//     console.error(`Sortie d'erreur : ${stderr}`);
 // });
 
 exec(svcList, (erreur, stdout, stderr) => {
     if (erreur) {
-        console.error(Erreur lors de la récupération de la liste des services : ${erreur});
+        console.error(`Erreur lors de la récupération de la liste des services : ${erreur}`);
         return;
     }
 
-    console.log(Liste des services : \n${stdout});
-    console.error(Sortie d'erreur : ${stderr});
+    console.log(`Liste des services : \n${stdout}`);
+    console.error(`Sortie d'erreur : ${stderr}`);
 });
